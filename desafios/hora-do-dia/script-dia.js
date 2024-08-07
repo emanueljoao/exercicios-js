47e9c8208cbb00694d474277f6675ba15c0e1f38
João Emanuel <emanueljoaoss123@gmail.com>
2024-08-07 20:09:38 -0300

    Só algumas mudanças

--- desafios/hora-do-dia/script-dia.js
+++ desafios/hora-do-dia/script-dia.js
@@ -2,6 +2,16 @@ var carregar = window.document.querySelector('body')
 carregar.addEventListener('load', carregar)
 
 function carregar(){
-    
+    var hora = new Date()
+    var hora_real = hora.getHours() 
+    var mensagem = window.document.querySelector('div.div-texto')
+    if (hora_real < 12) {
+
+        
+    } else if (hora_real >= 12 && hora_real <= 18) {
+
+    } else {
+
+    }
 
 }

870a24b75fad58d29bfe3a875a8bee2b232f6235
João Emanuel <emanueljoaoss123@gmail.com>
2024-08-12 11:09:47 -0300

    término do exercicio do conta-passos e inicio do exercicio da tabuada

--- desafios/conta-passos/script-conta-passos.js
+++ desafios/conta-passos/script-conta-passos.js
@@ -1,31 +1,37 @@
 var contar = document.querySelector('button')
 contar.addEventListener('click', verificar)
 
+var inicio = document.querySelector('input#inicio')
+var fim = document.querySelector('input#fim')
+var passo = document.querySelector('input#passo')
+var mensagem = document.querySelector('p')
+
 function verificar(reload) {
     reload.preventDefault()
-    var inicio = document.querySelector('input#inicio')
-    var começo = Number(inicio.value)
-    var fim = document.querySelector('input#fim')
-    var passo = document.querySelector('input#passo')
-    var mensagem = document.querySelector('p')
-    if (inicio === null || fim === null || passo === null) {
-        
-        window.alert = '[ERRO] Alguma informação não foi selecionada'
-        
-    } else if (Number(passo) > 0) {
-        
-        window.alert = 'Selecione um Numero maior que 0'
-        mensagem.innerHTML = 'Impossivel Contar'
-        
+    console.log(inicio.value, fim.value, passo.value)
+
+    if (inicio.value === '' || fim.value === '' || passo.value === '') {
+
+        window.alert('[ERRO] Alguma informação não foi selecionada')
+
     } else {
-        
-        mensagem.innerHTML = '<p>Contando</p>'
-        console.log(começo)
-        for (começo; começo < Number(fim); começo + Number(passo)) {
-            
-            mensagem.innerHTML += `${começo}`
+
+        let inicio_n = Number(inicio.value)
+        let fim_n = Number(fim.value)
+        let passo_n = Number(passo.value)
+
+        mensagem.innerHTML = '<p>Contando...</p>'
+        for (let resultado = inicio_n; resultado <= fim_n; resultado += passo_n) {
+
+            mensagem.innerHTML += `${resultado} 👉`
 
         }
-    }
+        mensagem.innerHTML += `🏁`
 
+    }
 }
+
+// if (Number(inicio.value) < 0 || Number(fim.value) <= 1 || Number(passo.value) <= 0) {
+
+//     window.alert('INICIO - Sempre maior ou igual a 0 \n FIM - Sempre maior que 1 \n PASSO - Sempre maior que 0')
+// } 
